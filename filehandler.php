77090f57dc9107bdf905652a6d50dfb372c774b3
Nyolczas <nyolczas@gmail.com>
2019-06-14 23:08:15 +0200

    filehandler

--- filehandler.php
+++ filehandler.php
@@ -11,9 +11,24 @@ function writeCsv( $line ) {
 writeCsv('2019.03.04.;230;359');
 
 // tömbbé alakítja az adatot.
+$first = true;
  if (($handle = fopen('data.csv', 'r')) !== FALSE) { // Check the resource is valid
+    echo ('<table>'); 
      while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) { // Check opening the file is OK!
-         print_r($data); // Array
+        echo('<tr>');
+        if($first) {
+            foreach ($data as $value) {
+                echo('<th>'.$value.'</th>');
+            }
+            $first=false;
+        } else {
+            foreach ($data as $value) {
+                echo('<td>'.$value.'</td>');
+            }
+        }
+        echo('</tr>');
+         //print_r($data); // Array
    }
+   echo ('</table>'); 
      fclose($handle);
  }
